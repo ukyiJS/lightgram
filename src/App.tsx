80ac7033b92cb387b1d54b98420fb66d5844d6dc
ukyi <ukyi.js@gmail.com>
2024-03-25 01:08:53 +0900

    ✨ 파일분리, 실행취소 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { invoke } from '@tauri-apps/api';
+import { open } from '@tauri-apps/api/dialog';
+import './App.css';
+import { useState } from 'react';
+
+async function separateFilesByExtension(directoryPath: string): Promise<string> {
+  try {
+    return await invoke<string>('separate_files_by_extension', { dirPath: directoryPath });
+  } catch (error) {
+    console.error('Error separating file_features:', error);
+    return error as string;
+  }
+}
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [message, setMessage] = useState<string>('');
+  const handleSeparateFileClick = async () => {
+    const path = await open({
+      directory: true,
+      multiple: false,
+      title: '디렉토리 선택',
+    });
+    if (!path) return;
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    const resultMessage = await separateFilesByExtension(path as string);
+    setMessage(resultMessage);
+  };
+
+  const handleUndoClick = async () => {
+    const resultMessage = await invoke<string>('undo_files');
+    console.log('### resultMessage', resultMessage);
+  };
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    <main>
+      <button onClick={handleSeparateFileClick}>raw, jpg 분리</button>
+      <button onClick={handleUndoClick}>실행취소</button>
+      {message}
+    </main>
   );
 }
 
