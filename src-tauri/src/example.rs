3404c1f5ce08115b38fe3be587b73d36ca5b5da6
ukyi <ukyi.js@gmail.com>
2024-03-25 01:25:01 +0900

    🔥 example rs 제거

--- src-tauri/src/example.rs
+++
@@ -1,9 +0,0 @@
-#[tauri::command]
-pub fn example_data() -> String {
-    format!("example data")
-}
-
-#[tauri::command]
-pub fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
